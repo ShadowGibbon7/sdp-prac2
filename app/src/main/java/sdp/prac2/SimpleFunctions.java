87810f14254bab1e3a32003885f28256ba2f97d3
ShadowGibbon7 <80965279+ShadowGibbon7@users.noreply.github.com>
2023-04-18 11:53:24 +0200

    Task 6

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,17 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    
+
+    public static List<Integer> task6(List<Integer> list_1) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0, i < list_1.size()) {
+            if (i % 100 == 0) {
+                result.add(i); }
+            else {
+                int rounded = ((i / 100) + 1) * 100;
+                result.add(rounded);
+            } }
+        return result;
+    }
 }
